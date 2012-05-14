c3805a7601f6bc5db5c8aafb168275dbb0035eef
Phil Brown <phil@philipbrown.id.au>
2012-05-14 15:35:31 +1000

    Add AddressOverride support to SetExpressCheckoutRequest

--- lib/Tolerable/PayPal/Request/SetExpressCheckoutRequest.php
+++ lib/Tolerable/PayPal/Request/SetExpressCheckoutRequest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Tolerable\PayPal\Request;
 
+use \InvalidArgumentException;
+
 class SetExpressCheckoutRequest extends ExpressCheckoutRequest
 {
     const METHOD = 'SetExpressCheckout';
@@ -16,6 +18,7 @@ class SetExpressCheckoutRequest extends ExpressCheckoutRequest
     const ALLOW_NOTE       = 'ALLOWNOTE';
     const LOCALE_CODE      = 'LOCALECODE';
     const SHOW_SHIPPING    = 'NOSHIPPING';
+    const ADDRESS_OVERRIDE = 'ADDROVERRIDE';
     const CALLBACK         = 'CALLBACK';
     const CALLBACK_TIMEOUT = 'CALLBACKTIMEOUT';
     const CALLBACK_VERSION = 'CALLBACKVERSION';
@@ -23,6 +26,8 @@ class SetExpressCheckoutRequest extends ExpressCheckoutRequest
     protected $method = self::METHOD;
     
     protected $showShipping = self::DEFAULT_SHIPPING_ADDRESS;
+    
+    protected $addressOverride = false;
     
     protected $returnUrl;
     
@@ -52,6 +57,24 @@ class SetExpressCheckoutRequest extends ExpressCheckoutRequest
         return $this;
     }
     
+    public function setShowShipping($showShipping) {
+        switch ($showShipping) {
+            case self::DISPLAY_SHIPPING_ADDRESS :
+            case self::HIDE_SHIPPING_ADDRESS :
+            case self::DEFAULT_SHIPPING_ADDRESS :
+                $this->showShipping = (int) $showShipping;
+                break;
+            default :
+                throw new InvalidArgumentException($showShipping);
+        }
+        return $this;
+    }
+    
+    public function setAddressOverride($addressOverride = true) {
+        $this->addressOverride = (bool) $addressOverride;
+        return $this;
+    }
+    
     public function setCallback($callback)
     {
         $this->callback = (string) $callback;
@@ -72,6 +95,7 @@ class SetExpressCheckoutRequest extends ExpressCheckoutRequest
             self::ALLOW_NOTE       => 1,
             self::SHOW_SHIPPING    => $this->showShipping,
             self::LOCALE_CODE      => 'AU',
+            self::ADDRESS_OVERRIDE => $this->addressOverride ? 1 : 0,
             self::CALLBACK         => $this->callback,
             self::CALLBACK_TIMEOUT => $this->callbackTimeout,
             self::CALLBACK_VERSION => $this->callbackVersion
