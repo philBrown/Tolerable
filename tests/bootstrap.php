4b77696957cb5b3c9fbdee52fc8fae0ec29c0239
Phil Brown <phil@philipbrown.id.au>
2013-03-23 13:30:31 +1100

    User composer autoloader for everything. WROCKS!

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,3 @@
 <?php
 
-require_once __DIR__.'/../vendor/symfony-components/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-$loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Tolerable' => __DIR__.'/../lib',
-    'Symfony\Component' => __DIR__.'/../vendor/symfony-components',
-    'Guzzle'            => __DIR__.'/../vendor/guzzle/src'
-));
-$loader->register();
-
+$autoloader = require __DIR__ . '/../vendor/autoload.php';
